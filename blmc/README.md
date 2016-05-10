93105b0b1fefa070898037b95e5471f0f642e9a7
Robert Lee <rlee32@gatech.edu>
2016-05-10 03:43:28 -0400

    blmc first version done

--- blmc/README.md
+++ blmc/README.md
@@ -2,7 +2,7 @@
 
 Arduino code for controlling brushless motors. A wired implementation (as opposed to wireless).  
 
-The Arduino processes input from a handheld device such as a joystick and outputs a PWM signal to an ESC.  
+The Arduino processes potentiometer input from a handheld device such as a joystick and outputs a PWM signal to an ESC.  
 
 
 
---
+++ blmc/main.ino
@@ -0,0 +1,85 @@
+// Author: Robert Lee
+// Email: rlee32@gatech.edu
+#include <Servo.h>
+
+// Pins.
+const int PIN_MOTOR = 11; // pwm-capable output pin.
+const int PIN_POT = 0; // potentiometer analog input pin.
+const int PIN_SAFETY = 1; // motor-on digital input pin.
+// Potentiometer constants.
+// constrain() is used on range, so ADC_MAX should be a little lower than 
+//  actual max, and ADC_MIN should be a little higher than actual min.
+// We divide signal values into finite levels to prevent jumpiness.
+const int ADC_MAX = 900; // ADC units (0 to 1023, inclusive).
+const int ADC_MIN = 100; // ADC units (0 to 1023, inclusive).
+const int ADC_LEVELS = 15; // We divide signal values into finite levels.
+// Pulse width constants.
+const int WIDTH_MAX = 2000; // microseconds.
+const int WIDTH_MIN = 1000; // microseconds.
+const int WIDTHS[ADC_LEVELS];
+// Other.
+const int LOOP_DELAY = 50; // Just in case the update is too frequent.
+
+Servo motor;
+
+void setup()
+{
+  pinMode(PIN_POT, INPUT);
+  pinMode(PIN_SAFETY, INPUT);
+  motor.attach(PIN_MOTOR);
+  // Initialize pulse width discretization.
+  int width_increment = ( WIDTH_MAX - WIDTH_MIN ) / ( ADC_LEVELS - 1 );
+  for( int i=0; i<ADC_LEVELS; ++i ) WIDTHS[i] = WIDTH_MIN + i*width_increment;
+  WIDTHS[0] = WIDTH_MIN;
+  WIDTHS[ADC_LEVELS-1] = WIDTH_MAX;
+}
+
+// Sets the ESC pulse width according to pot input.
+void throttle()
+{
+  int level = map( analogRead( PIN_POT ),
+    ADC_MIN, ADC_MAX,
+    0, ADC_LEVELS-1 );
+  level = constrain( level, 0, ADC_LEVELS-1 );
+  motor.writeMicroseconds( WIDTHS[level] );
+}
+
+void off() { motor.writeMicroseconds( WIDTH_MIN ); }
+
+void loop()
+{
+  if( digitalRead( PIN_SAFETY ) == HIGH )
+  {
+    throttle();
+  }
+  else
+  {
+    off();
+  }
+  delay( LOOP_DELAY );
+  // debug();
+}
+
+void debug()
+{
+  // switch ( received )
+  // {
+  //   case ',':
+  //     //Serial.print("Azimuth: ");
+  //     //Serial.println(pulse);
+  //     break;
+  //   case ';':
+  //     //Serial.print("Elevation: ");
+  //     //Serial.println(pulse);
+  //     break;
+  //   case '.':
+  //     //Serial.print("C1: ");
+  //     //Serial.println(pulse);
+  //     break;
+  //   case ':':
+  //     break;
+  // }
+  // delay(1000);
+}
+
+
