10b9397e393f43f0788416bbf9a822856bdfcc92
Robert Lee <rlee32@gatech.edu>
2016-07-31 09:31:51 -0400

    working load cell code

--- load-cell/load-cell.ino
+++ load-cell/load-cell.ino
@@ -1,43 +1,88 @@
 // Author: Robert Lee
 // Email: rlee32@gatech.edu
 
-// You must install the HX711 library first. It is easy.
-// It can be installed from the Arduino IDE via 
-// 'Sketch' > 'Manage Libraries...' and searching for the HX711 library. 
+// You must install the HX711 library first. 
+//  It can be easily installed from the Arduino IDE via 
+//  'Sketch' > 'Include Library' > 'Manage Libraries...' 
+//  and searching for the HX711 library. 
 // After installation, the appropriate header can be added via 
 // the 'Include Library...' popout menu. 
 #include <Q2HX711.h>
+// This is a wrapper class for the basic library.
+// Set constants in the base class.
+// #include "Sensor.h"
 
-// Amplified signal value to physical value
-const static double CALIBRATION_CONSTANT = -7050.0;
-
+// Parameters.
+// Proportionality constant of 
+//  amplified signal value to physical value.
+const static double CALIBRATION_CONSTANT = 0.00002;
 // Highest pin in a 4-pin connector.
 // Pins adjacent in this order: VCC,DAT,CLK,GND 
 const static int VCC_PIN = 11;
+const static int TARE_READINGS = 100;
+const static int WEIGHT_READINGS = 100;
+const static int POWER_ON_DELAY = 1000; // ms
 
-
-Q2HX711 scale(DAT_PIN, CLK_PIN);
+// Derived variables.
+const static int DAT_PIN = VCC_PIN-1;
+const static int CLK_PIN = VCC_PIN-2;
+const static int GND_PIN = VCC_PIN-3;
+Q2HX711 sensor(DAT_PIN, CLK_PIN);
+static double offset = 0;
 
 void setup()
 {
+  pinMode(VCC_PIN,OUTPUT); // VCC
+  //pinMode(DAT_PIN,INPUT); // DAT
+  //pinMode(CLK_PIN,INPUT); // CLK
+  pinMode(GND_PIN,OUTPUT); // GND
+  digitalWrite(VCC_PIN, HIGH);
+  digitalWrite(GND_PIN, LOW);
+  // Put a delay after setting VCC and GND pins 
+  //  (powering on the load cell amplifier),
+  //  so that readings are accurate.
   Serial.begin(9600);
   Serial.println("HX711 scale demo");
-  Serial.println("Readings:");
-  pinMode(VCC_PIN,OUTPUT); // VCC
-  pinMode(VCC_PIN-1,OUTPUT); // DAT
-  pinMode(VCC_PIN-2,OUTPUT); // CLK
-  pinMode(VCC_PIN-3,OUTPUT); // GND
+  Serial.print("Startup noise delay... ");
+  delay(POWER_ON_DELAY);
+  Serial.println("Done.");
+  tare();
 }
 
 void loop()
 {
-  if( scale.readyToSend() )
-  {
-    Serial.print("Reading: ");
-    Serial.print( scale.read() ); //scale.get_units() returns a float
-    Serial.print(" lbs");
-    Serial.println();
-  }
+  Serial.print("Reading: ");
+  Serial.print( weight(WEIGHT_READINGS) ); //scale.get_units() returns a float
+  Serial.print(" lbs");
+  Serial.println();
+ 
 }
 
-
+double average(double readings)
+{
+  double sum = 0;
+  int i = 0;
+  while(i < readings)
+  {
+    sum += readyRead();
+    ++i;
+  }
+  return sum / readings;
+}
+double weight()
+{
+  return (readyRead() - offset)*CALIBRATION_CONSTANT;
+}
+double weight(int readings)
+{
+  return (average(readings) - offset)*CALIBRATION_CONSTANT;
+}
+void tare()
+{
+  offset = average(TARE_READINGS);
+}
+double readyRead()
+{
+  while(not sensor.readyToSend());
+  return sensor.read();
+}
