c3f678e41686b83cbdc7c1077c32e0a44dabdc52
Robert Lee <robhlee32@gmail.com>
2016-10-08 15:20:04 -0400

    reconfigured for dual sensor configuration

--- blmc-load-cell/secondSensor/secondSensor.ino
+++ blmc-load-cell/secondSensor/secondSensor.ino
@@ -1,7 +1,7 @@
 #include "LoadCell.h"
 
-LoadCell loadCell(A5, -1, 0.00001010916, -1);
-// LoadCell loadCell(A5, -1, 0.00000998086, -1);
+//LoadCell loadCell(A3, -1, 0.00001010916, -1);
+LoadCell loadCell(A3, -1, 0.00000998086, -1);
 
 void setup()
 {
